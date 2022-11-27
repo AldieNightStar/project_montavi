e5d58d5813780a4b407dfad99e8e77c02183cf74
AldieNightStar <Aldie.NightStar@gmail.com>
2022-11-27 14:16:42 +0100

    update

--- api/seq.js
+++ api/seq.js
@@ -2,7 +2,7 @@ async function seq(array, caller=printLetter, prevName="<<", nextName=">>") {
     for (let i = 0; i < array.length;) {
         let elem = array[i];
         await caller(elem);
-        let direction = await choose("[" + (i+1) + "/" + array.length + "]", [["<<", 0], [">>", 1]])
+        let direction = await choose("[" + (i+1) + "/" + array.length + "]", [[prevName, 0], [nextName, 1]])
         if (direction == 0) {
             i-=1;
         } else {
